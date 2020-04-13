cdbb0132d2f4971e5a141d2a2236f8875aa22b87
Felipe Duque <felipe@duque.dev>
2020-04-13 19:29:15 -0300

    Desafio 1 - Concluído

--- desafio01/index.js
+++ desafio01/index.js
@@ -7,8 +7,8 @@ projects = [];
 
 function projectExists(req, res, next) {
   const { id } = req.params;
-  const projectID =  projects.find(project => project.id == id);
-  if(!projectID){
+  const project =  projects.find(p => p.id === id);
+  if(!project){
     return res.send(400).json("ID não encontrado");
   }
   return next();
@@ -24,11 +24,12 @@ server.get("/projects", countRequest ,(req, res) =>{
   return res.json(projects)
 })
 
-// //Lista um único projeto
-// server.get("/projects/:id", projectExists ,(req, res) =>{
-//   const { id } = req.params;
-//   return res.json(projects);
-// })
+//Lista um único projeto
+server.get("/projects/:id", projectExists ,(req, res) =>{
+  const { id } = req.params;
+  const project = projects.find(p => p.id === id)
+  return res.json(project);
+})
 
 //Cria um projeto
 server.post("/projects",countRequest,(req, res) => {
@@ -42,7 +43,7 @@ server.post("/projects",countRequest,(req, res) => {
 server.put("/projects/:id", projectExists, countRequest, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  const project =  projects.find(project => project.id === id);
+  const project =  projects.find(p => p.id === id);
   project.title = title
 
   res.send(projects);
@@ -51,9 +52,11 @@ server.put("/projects/:id", projectExists, countRequest, (req, res) => {
 //Deleta Usuário
 server.delete("/projects/:id", projectExists, countRequest, (req, res) => {
   const { id } = req.params;
-  const project =  projects.findIndex(project => project.id === id); //Não conhecia
+  const project =  projects.findIndex(p => p.id === id); //Não conhecia
   projects.splice(project, 1);
   return res.send(200);
 });
+
+//Adiciona uma task
 
 server.listen(3333); // Porta de Desafios
